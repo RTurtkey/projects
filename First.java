23c6ba97593b10044895c5267e83cce746913c13
RTurtkey <RadneyTurkson5@gmail.com>
2026-08-31 23:25:59 -0400

    Add files via upload

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First { // RADNEY TURKSON
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo!"); // This strings is whats being printed on the console
+        System.out.println("Job title: information security analyst"); 
+        System.out.println("Why: because I enjoy using technology and everything to do with cybersecurity");
+        System.out.println("Median salary: $124,910");
+    }
+}
