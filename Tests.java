cda24892beb0d4f10e19d36577cd6528da3eca65
RTurtkey <RadneyTurkson5@gmail.com>
2026-09-03 20:57:59 -0400

    Add files via upload

--- Tests.java
+++ Tests.java
@@ -1,1 +1,16 @@
-
+public class Tests {
+    public static void main(String[] args) {
+      double test1 = 56.8;
+      double test2 = 87.1;
+      double test3 = 99.9;
+      
+      double average = (test1 + test2 + test3) / 3;
+     
+      System.out.println("Test score 1: " + test1);
+    System.out.println("Test score 2: " + test2);
+    System.out.println("Test score 3: " + test3);
+    System.out.printf("The average of 3 test scores is: %.2f%n", average);
+}
+    
+   
+}
